c1a4da1f5dc9d7050edb6329bb4b1a76c0c6deb6
tatsuki-s <zekoushn@gmail.com>
2025-07-21 22:18:14 +0900

    labelやacceptなど新規作成画面の最適化

--- Apps/Organizer/src/createEvent.tsx
+++ Apps/Organizer/src/createEvent.tsx
@@ -8,17 +8,16 @@ function createEvent () {
             <div className="w-full flex justify-center items-center flex-col mb-30">
                 
                 <div className="w-9/10">
-                    <p className="mt-4 mb-2">イベントの名前</p>
-                    <input type="text" className="bg-gray-100 text-gray-900 rounded-lg p-2.5 w-full text-sm focus:border-gray-400 focus:ring-2 focus:ring-gray-400 outline-none" placeholder="イベントの名称を入力"/>
-                    <p className="mt-6 my-2">イベントの終了日</p>
-                    <input type="date" className="bg-gray-100 text-gray-900 rounded-lg p-2.5 w-full text-sm focus:border-gray-400 focus:ring-2 focus:ring-gray-400 outline-none"/>
-                    <p className="mt-6 my-2">地図のアップロード</p>
-                    <input type="file" className="bg-gray-100 text-gray-900 rounded-lg p-2.5 w-full text-sm focus:border-gray-400 focus:ring-2 focus:ring-gray-400 outline-none"/>
-                    <p className="mt-6 my-2">概要</p>
-                    <input type="text" className="bg-gray-100 text-gray-900 rounded-lg p-2.5 w-full h-30 text-sm focus:border-gray-400 focus:ring-2 focus:ring-gray-400 outline-none" placeholder="イベントの概要を入力"/>
-                    <p className="mt-6 my-2">サムネイルのアップロード</p>
-                    <input type="file" className="bg-gray-100 text-gray-900 rounded-lg p-2.5 w-full text-sm focus:border-gray-400 focus:ring-2 focus:ring-gray-400 outline-none"/>
-
+                    <label htmlFor="eventName" className="mt-4 mb-2 block">イベントの名前</label>
+                    <input id="eventName" type="text" className="bg-gray-100 text-gray-900 rounded-lg p-2.5 w-full text-sm focus:border-gray-400 focus:ring-2 focus:ring-gray-400 outline-none" placeholder="イベントの名称を入力"/>
+                    <label htmlFor="date" className="mt-6 my-2 block">イベントの終了日</label>
+                    <input id="date" type="date" className="bg-gray-100 text-gray-900 rounded-lg p-2.5 w-full text-sm focus:border-gray-400 focus:ring-2 focus:ring-gray-400 outline-none"/>
+                    <label htmlFor="map" className="mt-6 my-2 block">地図のアップロード</label>
+                    <input id="map" type="file" accept="image/*" className="bg-gray-100 text-gray-900 rounded-lg p-2.5 w-full text-sm focus:border-gray-400 focus:ring-2 focus:ring-gray-400 outline-none"/>
+                    <label htmlFor="description" className="mt-6 my-2 block">概要</label>
+                    <textarea id="description" className="bg-gray-100 text-gray-900 rounded-lg p-2.5 w-full h-30 text-sm focus:border-gray-400 focus:ring-2 focus:ring-gray-400 outline-none" placeholder="イベントの概要を入力"/>
+                    <label htmlFor="thumbnail" className="mt-6 my-2 block">サムネイルのアップロード</label>
+                    <input id="thumbnail" type="file" accept="image/*" className="bg-gray-100 text-gray-900 rounded-lg p-2.5 w-full text-sm focus:border-gray-400 focus:ring-2 focus:ring-gray-400 outline-none"/>
                 </div>
                 <Link to="/checkpoints" className="fixed w-9/10 bottom-0 text-white text-center bg-blue-500 font-bold px-12 py-2 rounded-md my-4">イベントを作成</Link>
             </div>
