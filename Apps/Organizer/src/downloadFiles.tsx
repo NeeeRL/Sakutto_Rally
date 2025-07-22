b20c7541e91f8567e35d5434fa3f1534f4e1ab6f
tatsuki-s <zekoushn@gmail.com>
2025-07-22 19:57:05 +0900

    ダウンロード・アップロード周りのUIを作成

--- Apps/Organizer/src/downloadFiles.tsx
+++ Apps/Organizer/src/downloadFiles.tsx
@@ -5,7 +5,43 @@ function downloadFiles () {
             
             <Header text="ファイルをダウンロード"/>
             <div className="w-full flex justify-center items-center flex-col">
-                <p className="">HTML，設定JSON，QRコード</p>
+                <h2 className="font-bold text-lg text-center">ダウンロード可能なファイル</h2>
+                {/* HTMLファイルのダウンロード */}
+                <div className="flex items-center justify-center">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-11 bg-gray-200 rounded-lg p-1.5">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m0 12.75h7.5m-7.5 3H12M10.5 2.25H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z" />
+                    </svg>
+                    <div className="m-4 w-36">
+                        <p>HTMLファイル</p>
+                        <p className="text-xs text-gray-600">参加者用HTMLファイル</p>
+                    </div>
+                    <button className="bg-gray-200 rounded-xl h-8 px-4">ダウンロード</button>
+                </div>
+                <div className="flex items-center justify-center">
+                    {/* アイコン */}
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-11 bg-gray-200 rounded-lg p-1.5">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M9.594 3.94c.09-.542.56-.94 1.11-.94h2.593c.55 0 1.02.398 1.11.94l.213 1.281c.063.374.313.686.645.87.074.04.147.083.22.127.325.196.72.257 1.075.124l1.217-.456a1.125 1.125 0 0 1 1.37.49l1.296 2.247a1.125 1.125 0 0 1-.26 1.431l-1.003.827c-.293.241-.438.613-.43.992a7.723 7.723 0 0 1 0 .255c-.008.378.137.75.43.991l1.004.827c.424.35.534.955.26 1.43l-1.298 2.247a1.125 1.125 0 0 1-1.369.491l-1.217-.456c-.355-.133-.75-.072-1.076.124a6.47 6.47 0 0 1-.22.128c-.331.183-.581.495-.644.869l-.213 1.281c-.09.543-.56.94-1.11.94h-2.594c-.55 0-1.019-.398-1.11-.94l-.213-1.281c-.062-.374-.312-.686-.644-.87a6.52 6.52 0 0 1-.22-.127c-.325-.196-.72-.257-1.076-.124l-1.217.456a1.125 1.125 0 0 1-1.369-.49l-1.297-2.247a1.125 1.125 0 0 1 .26-1.431l1.004-.827c.292-.24.437-.613.43-.991a6.932 6.932 0 0 1 0-.255c.007-.38-.138-.751-.43-.992l-1.004-.827a1.125 1.125 0 0 1-.26-1.43l1.297-2.247a1.125 1.125 0 0 1 1.37-.491l1.216.456c.356.133.751.072 1.076-.124.072-.044.146-.086.22-.128.332-.183.582-.495.644-.869l.214-1.28Z" />
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                    </svg>
+                    <div className="m-4 w-36">
+                        <p>設定ファイル</p>
+                        <p className="text-xs text-gray-600">バックアップ用</p>
+                    </div>
+                    <button className="bg-gray-200 rounded-xl h-8 px-4">ダウンロード</button>
+                </div>
+                <div className="flex items-center justify-center">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-11 bg-gray-200 rounded-lg p-1.5 ">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 4.875c0-.621.504-1.125 1.125-1.125h4.5c.621 0 1.125.504 1.125 1.125v4.5c0 .621-.504 1.125-1.125 1.125h-4.5A1.125 1.125 0 0 1 3.75 9.375v-4.5ZM3.75 14.625c0-.621.504-1.125 1.125-1.125h4.5c.621 0 1.125.504 1.125 1.125v4.5c0 .621-.504 1.125-1.125 1.125h-4.5a1.125 1.125 0 0 1-1.125-1.125v-4.5ZM13.5 4.875c0-.621.504-1.125 1.125-1.125h4.5c.621 0 1.125.504 1.125 1.125v4.5c0 .621-.504 1.125-1.125 1.125h-4.5A1.125 1.125 0 0 1 13.5 9.375v-4.5Z" />
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 6.75h.75v.75h-.75v-.75ZM6.75 16.5h.75v.75h-.75v-.75ZM16.5 6.75h.75v.75h-.75v-.75ZM13.5 13.5h.75v.75h-.75v-.75ZM13.5 19.5h.75v.75h-.75v-.75ZM19.5 13.5h.75v.75h-.75v-.75ZM19.5 19.5h.75v.75h-.75v-.75ZM16.5 16.5h.75v.75h-.75v-.75Z" />
+                    </svg>
+                    <div className="m-4 w-36">
+                        <p>QRコード</p>
+                        <p className="text-xs text-gray-600">QRコードの画像ファイル</p>
+                    </div>
+                    <button className="bg-gray-200 rounded-xl h-8 px-4">ダウンロード</button>
+                </div>
+
+                <button className="fixed w-9/10 bottom-0 text-white text-center bg-blue-500 font-bold px-12 py-2 rounded-md my-4">すべてを一括ダウンロード</button>
             </div>
         </>
     )
--- Apps/Organizer/src/loadFiles.tsx
+++ Apps/Organizer/src/loadFiles.tsx
@@ -1,9 +1,19 @@
 import Header from './header.tsx'
+import { Link } from 'react-router-dom'
 function loadFiles () {
     return(
         <>
             <Header text="設定ファイルを読み込む"/>
-            <p className="">ロード画面</p>
+            <div className="w-full flex justify-center items-center flex-col mb-30">                
+                <div className="w-9/10">
+                    <h2 className="font-bold text-lg text-center">設定ファイルをアップロードしてください</h2>
+                    <p className="my-14">事前にスタンプラリーを作成したことがある場合，設定ファイルがあれば使用することができます。</p>
+                    <label htmlFor="settingsFile" className="mt-6 my-2 block">設定ファイルのアップロード</label>
+                    <input id="settingsFile" type="file" accept="application/json" className="bg-gray-100 text-gray-900 rounded-lg mb-24 p-2.5 w-full h-32 text-sm focus:border-gray-400 focus:ring-2 focus:ring-gray-400 outline-none"/>
+
+                </div>
+                <Link to="/download" className="fixed w-9/10 bottom-0 text-white text-center bg-blue-500 font-bold px-12 py-2 rounded-md my-4">アップロードする</Link>
+            </div>
         </>
     )
 }
