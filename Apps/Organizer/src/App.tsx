4d9ac51d5ee8fdab6510dd519edc6ebee957c2c4
tatsuki-s <zekoushn@gmail.com>
2025-08-14 22:54:14 +0900

    nullでJSONを保存してくれるようにした

--- Apps/Organizer/src/App.tsx
+++ Apps/Organizer/src/App.tsx
@@ -3,15 +3,31 @@ import { Link } from 'react-router-dom'
 import './App.css'
 
 function App() {
+  const generateNullJson = () => {    
+    const nullJson = {
+      eventName: null,
+      endDate: null,
+      description: null,
+      checkpoints: [ null ]
+    }
+    
+    localStorage.setItem("eventData", JSON.stringify(nullJson))
+    console.log("nullJson is saved", nullJson)
+  }
 
-  // const [count, setCount] = useState(0)
 
   return (
     <>
       <p className="">適当な画像</p>
       <p>スタンプラリーをサクッとミニマムに作成できます！</p>
       <div className="w-full flex justify-center items-center flex-col">
-        <Link to="/create" className="text-white text-center bg-blue-400 font-bold px-12 py-2 rounded-md my-4">新しく作成する</Link>
+        <Link 
+          to="/create" 
+          className="text-white text-center bg-blue-400 font-bold px-12 py-2 rounded-md my-4"
+          onClick={generateNullJson}
+        >
+          新しく作成する
+        </Link>
         <Link to="/load" className="text-white text-center bg-blue-400 font-bold px-12 py-2 rounded-md my-4">設定ファイルを読み込む</Link>
       </div>
     </>
