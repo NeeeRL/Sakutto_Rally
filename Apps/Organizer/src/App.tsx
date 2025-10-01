60365f3fea3a7eb7a7ca4ba4cb2f22c516997737
NeeeRL <kenken.s5049@gmail.com>
2025-10-01 22:34:20 +0900

    見た目を変更

--- Apps/Organizer/src/App.tsx
+++ Apps/Organizer/src/App.tsx
@@ -7,19 +7,42 @@ function App() {
 
   return (
     <>
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-full size-64 mx-auto">
-      <path strokeLinecap="round" strokeLinejoin="round" d="M9 6.75V15m6-6v8.25m.503 3.498 4.875-2.437c.381-.19.622-.58.622-1.006V4.82c0-.836-.88-1.38-1.628-1.006l-3.869 1.934c-.317.159-.69.159-1.006 0L9.503 3.252a1.125 1.125 0 0 0-1.006 0L3.622 5.689C3.24 5.88 3 6.27 3 6.695V19.18c0 .836.88 1.38 1.628 1.006l3.869-1.934c.317-.159.69-.159 1.006 0l4.994 2.497c.317.158.69.158 1.006 0Z" />
-    </svg>
-      <p>スタンプラリーをサクッとミニマムに作成できます！</p>
-      <div className="w-full flex justify-center items-center flex-col">
-        <Link 
-          to="/create" 
-          className="text-white text-center bg-blue-400 font-bold px-12 py-2 rounded-md my-4"
-          // onClick={generateNullJson}
-        >
-          新しく作成する
-        </Link>
-        <Link to="/load" className="text-white text-center bg-blue-400 font-bold px-12 py-2 rounded-md my-4">設定ファイルを読み込む</Link>
+      <h1 className='mt-4 mb-2 text-4xl text-center'>Sakutto Rally</h1>
+      <p className='text-center mb-4'>～気軽に運用できるスタンプラリーメーカー～​</p>
+      <img src="../img/honbann.svg" alt="" className='w-[90%] h-auto m-auto  ' />
+      <p className='text-center w-[84%] m-auto'>スタンプラリーイベントをサクッと気軽に開催できる、ミニマムなスタンプラリーメーカーです。サーバーさえあればファイルの作成も運用にもコストがかかりません。</p>
+      <div className="w-full flex justify-center items-center flex-col my-8">
+        <div className='my-4 flex justify-center items-center flex-col bg-gray-100 w-[84%] py-4 rounded-2xl'>
+          
+          <div className='flex justify-center items-center gap-4'>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+            </svg>
+            <p className='text-xl my-2'>初めての方はこちら</p>
+          </div>
+          <p className='w-[80%] m-auto text-center'>スタンプラリーファイルを新しく生成することができます。</p>
+          <Link 
+            to="/create" 
+            className="text-white text-center bg-blue-400 font-bold px-12 py-2 rounded-md mb-2 mt-4"
+            // onClick={generateNullJson}
+          >
+            新しく作成する
+          </Link>
+        </div>
+
+        <div className='my-4 flex justify-center items-center flex-col bg-gray-100 w-[84%] py-4 rounded-2xl'>
+          
+          <div className='flex justify-center items-center gap-4'>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="m9 13.5 3 3m0 0 3-3m-3 3v-6m1.06-4.19-2.12-2.12a1.5 1.5 0 0 0-1.061-.44H4.5A2.25 2.25 0 0 0 2.25 6v12a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 18V9a2.25 2.25 0 0 0-2.25-2.25h-5.379a1.5 1.5 0 0 1-1.06-.44Z" />
+</svg>
+
+            <p className='text-xl my-2'>データを読み込む</p>
+          </div>
+          <p className='w-[80%] m-auto text-center'>設定ファイルを読み込めば、以前使用した時のデータを復元できます。</p>
+          <Link to="/load" className="text-white text-center bg-blue-400 font-bold px-12 py-2 rounded-md mb-2 mt-4">設定ファイルを読み込む</Link>
+        </div>
+
       </div>
     </>
   )
