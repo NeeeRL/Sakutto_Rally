6723a991f3a1005e9c5ecc336b4490fa7d0cd28a
tatsuki-s <zekoushn@gmail.com>
2025-10-20 10:37:52 +0900

    saveアイコンをそれっぽく変更

--- Apps/Organizer/src/createEvent.tsx
+++ Apps/Organizer/src/createEvent.tsx
@@ -311,7 +311,7 @@ const createEvent = () => {
                         className="w-auto text-white text-center font-bold px-2 py-2 rounded-md my-4 bg-blue-500 ml-8"
                         onClick={() => {saveInput(); saveChangeIcon();}}
                     >
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 rotate-270">
                             <path strokeLinecap="round" strokeLinejoin="round" d={saveIcon} />
                         </svg>
                     </button>
