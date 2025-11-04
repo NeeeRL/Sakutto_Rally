f085c65cf98714b588ae53c90ebe827b4e81565d
tatsuki-s <zekousn@gmail.com>
2025-11-04 09:46:25 +0900

    一括ダウンロードロジックの間違いを修正

--- Apps/Organizer/src/downloadFiles.tsx
+++ Apps/Organizer/src/downloadFiles.tsx
@@ -428,7 +428,7 @@ function downloadFiles () {
 
                 <button
                     className="bottom-0 text-white text-center bg-blue-500 font-bold px-12 py-2 rounded-md my-4"
-                    onClick={() => {downloadJsonFile(); downloadQRCode();}}
+                    onClick={() => {generateHTML(); downloadJsonFile(); downloadQRCode();}}
                 >
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 inline">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3" />
